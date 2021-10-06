191a8ac75a0d5952853b721b2c949b9dbc934a72
Jan Gimenez Layola <gilajan@protonmail.com>
2021-10-06 09:44:54 +0200

    Refactor numbers initialitzation

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,10 +6,7 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var numbers = new double[3];
-            numbers[0] = 12.7; 
-            numbers[1] = 10.3; 
-            numbers[2] = 6.11; 
+            var numbers = new [] { 12.7, 10.3, 6.11 };
 
             var result = numbers[0];
             result = result + numbers[1];
