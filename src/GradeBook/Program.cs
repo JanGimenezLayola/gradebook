3dc2f06bccaa0618104ae1e9ce0c45db2874206d
Jan Gimenez Layola <gilajan@protonmail.com>
2021-10-06 09:54:23 +0200

    Refactor numbers initialitzation to List

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,16 +1,16 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace GradeBook
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var numbers = new [] { 12.7, 10.3, 6.11 };
-
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
             
             var result = 0.0;
-            foreach(double number in numbers)
+            foreach(double number in grades)
             {
                 result += number;
             }
