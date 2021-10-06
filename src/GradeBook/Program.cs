3f21c7f9d10a2dbaa0591a5e327922dc1b0dfe82
Jan Gimenez Layola <gilajan@protonmail.com>
2021-10-06 09:37:01 +0200

    Add sum operation

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,6 +6,12 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            double x = 34.1; // can initializa as var 
+            double y = 10.3;
+            double result = x + y;
+            
+            Console.WriteLine(result);
+            
             if(args.Length > 0) 
             {
                 Console.WriteLine($"Hello {args[0]}!");
