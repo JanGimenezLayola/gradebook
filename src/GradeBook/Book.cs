3b76d7013748f7e7015e97fef71e6412b28c325e
JanGimenezLayola <gilajan@protonmail.com>
2021-10-07 15:00:06 +0200

    Refactor show statistics as Book method

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 namespace GradeBook
 
@@ -11,11 +12,32 @@ namespace GradeBook
             this.name = name;
         }
 
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var lowGrade = double.MaxValue;
+            var highGrade = double.MinValue;
+
+            foreach(double number in grades)
+            {
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
+                result += number;
+            }
+
+            Console.WriteLine($"The sum grades is {result}");
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The higher grade is {highGrade}");
+        
+            var average = result / grades.Count;
+            Console.WriteLine($"The average grade is {average:N1}"); // :N3 -> round to three decimal places
+        }
+
         public void AddGrade(double grade)
         {
             grades.Add(grade);
         }
-        
+
         private List<double> grades;
         private string name;
     
