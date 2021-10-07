ea7b9ff79fc15f14c7f26d314795b527483ff743
JanGimenezLayola <gilajan@protonmail.com>
2021-10-07 14:49:20 +0200

    Add check highGrade logic

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,13 +9,20 @@ namespace GradeBook
     
             var book = new Book("Jan's Grade Book");
             book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
 
             var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
             
             var result = 0.0;
+            var highGrade = double.MinValue;
+
             foreach(double number in grades)
             {
+                if(number > highGrade) {
+                    highGrade = number;
+                }
                 result += number;
             }
             Console.WriteLine($"The sum grades is {result}");
