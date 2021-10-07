b7845b4e2d46415ce1a6573db94ff82593f0c235
JanGimenezLayola <gilajan@protonmail.com>
2021-10-07 14:40:36 +0200

    Add Book name property and set it at constructor

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -5,16 +5,19 @@ namespace GradeBook
     class Book
     {
 
-        public Book()
+        public Book(string name)
         {
             grades = new List<double>();
+            this.name = name;
         }
 
         public void AddGrade(double grade)
         {
             grades.Add(grade);
         }
-        List<double> grades = new List<double>();
+        
+        private List<double> grades;
+        private string name;
     
     }
 }
